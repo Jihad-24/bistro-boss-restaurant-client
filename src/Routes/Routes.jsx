adea0ad1d46b1440dadefd7e30dc446cadab823e
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-22 20:03:26 +0600

    url server

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -94,7 +94,7 @@ const router = createBrowserRouter([
             {
                 path: 'updateItem/:id',
                 element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/menu/${params.id}`),
+                loader: ({ params }) => fetch(`https://bistro-boss-restaurant-server-jihad.vercel.app/menu/${params.id}`),
             },
         ]
     }
